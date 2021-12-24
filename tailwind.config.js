55ed692a75423c22d2d5d35d5f019f48b8a02fe5
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2021-12-24 13:17:56 +0100

    :lipstick: Adding Fonts by Default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 const colors = require('tailwindcss/colors')
 
+const defaultFontFamily = {
+  'sans': ['ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+  'serif': ['ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
+  'mono': ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace']
+}
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -9,6 +15,11 @@ module.exports = {
     extend: {},
     colors: {
       ...colors
+    },
+    fontFamily: {
+      'sans': ['Pushster', ...defaultFontFamily['sans']],
+      'serif': [...defaultFontFamily['serif']],
+      'mono': [...defaultFontFamily['mono']],
     }
   },
   plugins: [],
